5254ecde5575809e2a54e9ea001821697a5a427d
Bhavyak20 <bhavyak.fullstack@gmail.com>
2025-05-15 22:53:43 +0530

    JS Practice for 14 and 15 May

---
+++ console.js
@@ -0,0 +1,4 @@
+console.log(9);
+console.log(10);
+document.write(9);
+window.alert(10)
---
+++ variables.js
@@ -0,0 +1,18 @@
+//var can be redeclare and reassign
+var age = 9;
+var age = 600
+console.log(age)
+
+
+
+//let can reassign but can't redeclare
+let customerdetails = 123456789
+    customerdetails= 0
+//let customerdetails = 999999999
+console.log(customerdetails)
+document.write(customerdetails)
+window.alert(customerdetails)
+
+//can't reassign & redeclare
+const place = 999
+console.log(place);
